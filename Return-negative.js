8fbd509e6888b4d83c8c7c6d1c24f1e271c6842a
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-09 20:42:06 +0000

    solved kata

--- Return-negative.js
+++ Return-negative.js
@@ -11,3 +11,7 @@
 
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+    return -Math.abs(num)
+  } 
