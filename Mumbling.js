46703753448fa610a6602fddfe5b98f0c82c24c2
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-10 18:53:10 +0000

    solved kata

--- Mumbling.js
+++ Mumbling.js
@@ -6,3 +6,16 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+	const letters = s.split('');
+  let result = '';
+  
+  for (let i = 0; i < letters.length; i++) {
+    result += letters[i].toUpperCase() + letters[i].toLowerCase().repeat(i);
+    if (i !== letters.length - 1) {
+      result += '-';
+    }
+  }
+  return result
+} 
