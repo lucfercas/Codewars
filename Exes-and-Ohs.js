48433c736ca04bb47ba923e5b2143cc4fe4de585
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-11 15:42:29 +0000

    solved kata

--- Exes-and-Ohs.js
+++ Exes-and-Ohs.js
@@ -8,3 +8,11 @@
 // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 // XO("zzoo") => false
 
+function XO(str) {
+    const filteredArray = Array.from(str.toLowerCase()).filter(char => char === 'x' || char === 'o');
+  
+    const xCount = filteredArray.filter(char => char === 'x').length;
+    const oCount = filteredArray.filter(char => char === 'o').length;
+  
+    return xCount === oCount;
+  }
