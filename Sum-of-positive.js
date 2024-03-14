420d550a18ebdea96c6b6da4b0c9d71a4dfaff25
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-14 21:24:06 +0000

    solved kata

--- Sum-of-positive.js
+++ Sum-of-positive.js
@@ -3,3 +3,13 @@
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 // Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] > 0) {
+       sum += arr[i];
+     }
+   }
+   return sum;
+ }
