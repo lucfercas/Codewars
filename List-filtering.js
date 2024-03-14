555551e1f881ad1efd236f2d02140e0ad5ccc445
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-14 21:20:58 +0000

    solved kata

--- List-filtering.js
+++ List-filtering.js
@@ -5,3 +5,8 @@
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+    const numbers = l.filter(item => typeof item === 'number');
+    return numbers
+    }
